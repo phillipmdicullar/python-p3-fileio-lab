1f0dd721b205fe02b2ea20002a124032ccb8bdba
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-12-04 16:46:54 +0300

    learned how to use append, read file, and write a file

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
-
+    file_name = (f"{file_name}.txt")
+    with open(file_name, mode="w", encoding="utf-8") as file:
+        file.write(file_content)
 def append_file(file_name, append_content):
+    file_name = (f"{file_name}.txt")
+    with open(file_name, mode="a", encoding="utf-8") as file:
+        file.write(append_content)
     pass
 
 def read_file(file_name):
+    file_name = (f"{file_name}.txt")
+    with open(file_name, mode="r", encoding="utf-8") as file:
+        return file.read()
     pass
+write_file(file_name="moringa", file_content="5 bananas added\n")
+append_file(file_name="moringa", append_content="3 bananas subtracted\n")
+
+content = read_file(file_name="moringa")
+print(content)
